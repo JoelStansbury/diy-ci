cc891ef5cb14d766ccbc3b978a73000d7d0ab1ab
JoelStansbury <48299585+JoelStansbury@users.noreply.github.com>
2022-08-21 19:29:07 -0400

    Update pyci_pipeline.py

--- pyci_pipeline.py
+++ pyci_pipeline.py
@@ -10,7 +10,7 @@ CONDA_BAT = os.environ["CONDA_BAT"]
 PYTHON = sys.executable
 
 def delete_previous_results():
-    call(["git", "filter-branch", "--tree-filter", "'git", "rm", "-f", "CI-results'", "HEAD"])
+    call(["git", "filter-branch", "--tree-filter", "'rm", "-f", "CI-results'", "HEAD"])
     call(["git", "push", "origin", "--force", "--all"])
 
 def on_success():
@@ -32,11 +32,11 @@ def on_failure():
 if __name__ == "__main__":
     print("now testing something else")
     try:
-        call([CONDA, "env", "update", "-f", "environment.yml"])
-        call([CONDA_BAT, "deactivate"])
-        call([CONDA_BAT, "activate", "./.venv"])
-        check_call(["pip", "install", "-e", ".", "--no-deps"])
-        check_call(["pip", "check"])
+        # call([CONDA, "env", "update", "-f", "environment.yml"])
+        # call([CONDA_BAT, "deactivate"])
+        # call([CONDA_BAT, "activate", "./.venv"])
+        # check_call(["pip", "install", "-e", ".", "--no-deps"])
+        # check_call(["pip", "check"])
         print("do some testing")
         # raise ValueError("what happens on an error")
         on_success()
