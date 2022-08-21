1a076269ba50ef6028542973fd6d6e2e5435392a
JoelStansbury <stansbury.joel@gmail.com>
2022-08-21 15:48:56 -0400

    pipeline example

--- pyci_pipeline.py
+++ pyci_pipeline.py
@@ -1,5 +1,23 @@
 import pyci
+from pathlib import Path
+from datetime import datetime
+
+def on_success():
+    with open("CI-results", "w") as f:
+        f.write(f"success: {datetime()}")
+def on_failure():
+    with open("CI-results", "w") as f:
+        f.write(f"failure: {datetime()}")
+
 
 if __name__ == "__main__":
     print("now testing something else")
-    raise ValueError("what happens on an error")
+    try:
+        print("do some testing")
+        # raise ValueError("what happens on an error")
+    except:
+        print("I expected that error")
+        on_failure()
+    on_success()
+    
+    
