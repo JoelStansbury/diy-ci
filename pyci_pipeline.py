88ea730e2e5a7735a84023b32eb69066a367bbf6
JoelStansbury <stansbury.joel@gmail.com>
2022-08-21 20:29:35 -0400

    revert working behaviour

--- pyci_pipeline.py
+++ pyci_pipeline.py
@@ -12,16 +12,12 @@ PYTHON = sys.executable
 def on_success():
     with open("CI-results", "w") as f:
         f.write(f"success: {datetime.now()}")
-    call(["git", "update-index", "--no-skip-worktree", "CI-results"])
-    call(["git", "update-index", "--assume-unchanged", "CI-results"])
     call(["git", "add", "CI-results"])
     call(["git", "commit", "-m", "pass"])
     call(["git", "push"])
 def on_failure():
     with open("CI-results", "w") as f:
         f.write(f"failure: {datetime.now()}")
-    call(["git", "update-index", "--no-skip-worktree", "CI-results"])
-    call(["git", "update-index", "--no-assume-unchanged", "CI-results"])
     call(["git", "add", "CI-results"])
     call(["git", "commit", "-m", "fail"])
     call(["git", "push"])
@@ -30,13 +26,13 @@ def on_failure():
 if __name__ == "__main__":
     print("now testing something else")
     try:
-        # call([CONDA, "env", "update", "-f", "environment.yml"])
-        # call([CONDA_BAT, "deactivate"])
-        # call([CONDA_BAT, "activate", "./.venv"])
-        # check_call(["pip", "install", "-e", ".", "--no-deps"])
-        # check_call(["pip", "check"])
+        call([CONDA, "env", "update", "-f", "environment.yml"])
+        call([CONDA_BAT, "deactivate"])
+        call([CONDA_BAT, "activate", "./.venv"])
+        check_call(["pip", "install", "-e", ".", "--no-deps"])
+        check_call(["pip", "check"])
         print("do some testing")
-        raise ValueError("what happens on an error")
+        # raise ValueError("what happens on an error")
         on_success()
     except Exception as e:
         print(e)
