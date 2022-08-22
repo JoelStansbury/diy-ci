deadeef9702d85885da9a75a02d23bc358b6ec5a
JoelStansbury <stansbury.joel@gmail.com>
2022-08-21 21:39:11 -0400

    minimize repo

--- example_pipeline.py
+++ example_pipeline.py
@@ -1,39 +1,38 @@
-import pyci
-from pathlib import Path
 from datetime import datetime
-from subprocess import call, check_call
-import sys
+from subprocess import call
 import os
 
-CONDA = os.environ["CONDA_EXE"]
-CONDA_BAT = os.environ["CONDA_BAT"]
-PYTHON = sys.executable
+class Conda:
+    """Maybe useful class"""
+    EXE = os.environ["CONDA_EXE"]
+    BAT = os.environ["CONDA_BAT"]
+    ENV_PATH = "./.venv"
+    ENV_FILE = "environment.yml"
 
-def on_success():
+    @classmethod
+    def update(self):
+        call([Conda.EXE, "env", "update", "-f", Conda.ENV_FILE, "-p", Conda.ENV_PATH])
+        call([Conda.BAT, "deactivate"])
+        call([Conda.BAT, "activate", Conda.ENV_PATH])
+
+def respond(message):
     with open("CI-results", "w") as f:
-        f.write(f"success: {datetime.now()}")
+        f.write(f"{message}: {datetime.now()}")
     call(["git", "add", "CI-results"])
-    call(["git", "commit", "-m", "pass"])
-    call(["git", "push"])
-def on_failure():
-    with open("CI-results", "w") as f:
-        f.write(f"failure: {datetime.now()}")
-    call(["git", "add", "CI-results"])
-    call(["git", "commit", "-m", "fail"])
+    call(["git", "commit", "-m", message])
     call(["git", "push"])
 
 
 if __name__ == "__main__":
     print("now testing something else")
     try:
-        call([CONDA, "env", "update", "-f", "environment.yml"])
-        call([CONDA_BAT, "deactivate"])
-        call([CONDA_BAT, "activate", "./.venv"])
-        check_call(["pip", "install", "-e", ".", "--no-deps"])
-        check_call(["pip", "check"])
-        print("do some testing")
+        print("do something like this")
+        # Conda.update()
+        # check_call(["pip", "install", "-e", ".", "--no-deps"])
+        # check_call(["pip", "check"])
+        # check_call(["pytest"])
         # raise ValueError("what happens on an error")
-        on_success()
+        respond("success")
     except Exception as e:
         print(e)
-        on_failure()
+        respond("failure")
